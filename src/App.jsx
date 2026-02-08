f58b9f99d582c3e9d977538ead7e4006a532b935
ShreyasVH <shreyas.deve.1@gmail.com>
2026-02-08 11:22:20 +0530

    suppoting multiple doughnut charts

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,46 @@ function App() {
     if (params.has('type')) {
         type = params.get('type');
     }
+
+    const dataDoughnut1 = {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [
+            {
+                label: 'My Doughnut Chart',
+                data: [40, 20, 12],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1
+            }
+        ]
+    };
+
+    const dataDoughnut2 = {
+        labels: [ 'April', 'May', 'June' ],
+        datasets: [ { label: 'My Doughnut Chart 2', data: [33, 25, 18],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1
+            }
+        ]
+    };
+
   return (
     <>
         <div style="margin-bottom: 30px">
@@ -20,7 +60,8 @@ function App() {
         </Show>
 
         <Show when={type === 'doughnut'}>
-            <DoughnutChart />
+            <DoughnutChart data={dataDoughnut1} />
+            <DoughnutChart data={dataDoughnut2} />
         </Show>
     </>
   )
